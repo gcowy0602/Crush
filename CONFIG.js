dc137dc02ba059b407ce33af3598e9c103282609
gcowy0602 <99636508+gcowy0602@users.noreply.github.com>
2022-02-14 13:26:01 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Toi tặng quà valen thì c có nhận hong",
+  titleHeader: "Toi tặng quà valentine thì H.Nhi có nhận hong",
   descriptionHeader:
-    "Nếu b thoát ra và không trả lời thì có nghĩa là b đã ĐỒNG Ý rồi đấy ^^",
+    "Nếu bà thoát ra và không trả lời thì có nghĩa là bà đã ĐỒNG Ý rồi nha =)))",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "Okela...",
   descriptionModar:
-    'Vì lý do địa lý nên hong gửi đc ^^ </br> Cho nợ đi nào gặp toi trả sau :)) </br> "Từ mai b học r nên khó rủ game đc. </br>  Chúc b học tốt "',
+    'Vì có ai đó ngại nên chắc hong gửi được ^^ </br> Cho nợ đi nào gặp toi trả sau :)) </br> 
 };
